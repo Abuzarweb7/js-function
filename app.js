af926423bd5aa324c8efe2a76cf256faf295b704
hammad-sohail <hammadsohail733@gmail.com>
2025-07-28 12:18:52 +0500

    fnc q 10

---
+++ app.js
@@ -0,0 +1,102 @@
+// Q1
+// Write a function that takes two numbers and returns their sum.
+// function sum(a ,b){
+//     return a + b;
+// }
+// let result = sum(10,20);
+// console.log(result);
+//Q2
+// Create a function that takes two numbers and returns the result of the first number
+// minus the second.
+// function minus(a,b){
+//     return a - b;
+// }
+// let minusResult =minus(40,20);
+// console.log(minusResult);
+//Q3
+// Write a function that multiplies two numbers and returns the result.
+// function multiply(a,b){
+//     return a * b;
+// }
+// let multiplyResult = multiply(10,5);
+// console.log(multiplyResult);
+
+//Q4
+// Create a function that takes two numbers and returns the result of dividing the first by
+// the second.
+// function divide(a,b){
+//     return a /b;
+// }
+// let divideResult =divide(200,50);
+// console.log(divideResult);
+// Q5
+// Write a function that takes two numbers and returns the remainder when the first
+// number is divided by the second.
+// function remaider(a,b){
+//     return a % b;
+// }
+// let remaniderResult = remaider(20,3);
+// console.log(remaniderResult);
+//Q6
+// Write a function that checks if a number is positive. If it is, return "Positive",
+// otherwise return "Not Positive".
+// function abcd(number){
+//     if(number > 0){
+//         return "Positive";
+//     }
+//     else{
+//         return "Not Positive";
+//     }
+// }
+// console.log(abcd(10));
+//Q7
+// Create a function that checks if a number is negative. If it is, return "Negative", else
+// return "Not Negative".
+//  function add(num){
+// if(num < 0){
+//     return "Negative"
+// }
+// else{
+//     return"Not Negative";
+// }
+//  }
+//  console.log(add(-10));
+//Q8
+// Write a function that takes a number and returns "Even" if the number is even,
+// otherwise "Odd".
+// function isEvenorOdd(num){
+//     if(num%2 === 0){
+//         return "Even";
+//     }
+//     else{
+//         return"Odd";
+//     }
+// }
+// console.log(isEvenorOdd(2));
+//Q9
+
+
+// Write a function that takes two numbers and returns "Equal" if they are the same,
+// otherwise "Not Equal".
+// function isEqual(num1,num2){
+// if(num1 === num2){
+//     return"Equal"
+// }
+// else{
+//     return "Not Equal";
+// }
+// }
+// console.log(isEqual(20,20));
+
+  //Q10
+// Create a function that returns "Greater" if the first number is greater than the second,
+// else return "Smaller".
+// function isGreater(num1,num2){
+//     if(num1 > num2){
+//         return "Greater";
+//     }
+//     else{
+//         return "Smaller";
+//     }
+// }
+// console.log(isGreater(20,10));
